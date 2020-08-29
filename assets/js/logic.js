9cb4a760a24a6793605d3ac688c1c71df66bd53d
AnnaPaliils <annapaliy@annas-air.myfiosgateway.com>
2020-08-29 09:49:06 -0400

    changes

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,20 +1,37 @@
   // variables to keep track of quiz state
-var currentQuestionIndex = 0;
+var questionsIndex = 0;
 var time = questions.length * 15;
 var timerId;
+var timerInterval;
 
 // variables to reference DOM elements
 var questionsEl = document.getElementById("questions");
-var timerEl = document.getElementById("time");
+var questionTitle = document.getElementById("question-title");
 var choicesEl = document.getElementById("choices");
+var timerEl = document.getElementById("time");
 var submitBtn = document.getElementById("submit");
 var startBtn = document.getElementById("start");
 var initialsEl = document.getElementById("initials");
 var feedbackEl = document.getElementById("feedback");
+var endScreen = document.getElementById("end-screen");
+var finalScore = document.getElementById("final-score");
 
 // sound effects
 var sfxRight = new Audio("assets/sfx/correct.wav");
 var sfxWrong = new Audio("assets/sfx/incorrect.wav");
+
+function setTime() {
+  timerInterval = setInterval(function() {
+  time--;
+  timerEl.innerHTML = time;
+
+    if(time=== 0) {
+      clearInterval(timerInterval);
+      quizEnd ();
+    }
+
+  }, 1000);
+}
 
 function startQuiz() {
   // hide start screen
@@ -23,82 +40,116 @@ function startQuiz() {
   console.log(startScreen);
   // un-hide questions section
   questionsEl.removeAttribute("class");
-  currentQuestionIndex= getQuestion;
-  console.losg(currentQuestionIndex);
+  getQuestion();
+  console.log(questionsIndex);
   // start timer
-  // setTimeout (function() {
-  // timerEl = time; 
-  // }, 75000);
-  // console.log(timerEl);
+  setTime (); 
   // show starting time
-  
+  timerEl.innerHTML = time;
 }
 
 function getQuestion() {
   // get current question object from array
 
   // update title with current question
+  questionTitle.innerHTML = questions[questionsIndex].title;
 
   // clear out any old question choices
-
+  choicesEl.innerHTML = "";
+  var choicesOpt = questions[questionsIndex].choices;
+  console.log(choicesOpt);
+  
   // loop over choices
-
+  for (i =0; i < choicesOpt.length; i++) {
     // create new button for each choice
+    var choiceBtn = document.createElement("button");
+    choiceBtn.textContent = choicesOpt[i];
+    choicesEl.appendChild(choiceBtn);
 
     // attach click event listener to each choice
-
-    // display on the page
+    choiceBtn.addEventListener("click", questionClick);
+  }
+  console.log(choicesEl);
+  // display on the page
 }
 
-function questionClick() {
+function questionClick(event) {
+  console.log(event.target.textContent);
+  console.log(questions[questionsIndex].answer);
   // check if user guessed wrong
+  if (questions[questionsIndex].answer !== event.target.textContent) {
     // penalize time
-
+    time = time - 10;
     // display new time on page
-
+    timerEl.innerHTML = time;
     // play "wrong" sound effect
-
+  sfxWrong.play ();
+  }
+  else {
   // else 
     // play "right" sound effect
-
-
-  // flash right/wrong feedback on page for half a second
-
+    sfxRight.play ();
+    // flash right/wrong feedback on page for half a second
+    // questions.createElement("<div>You are right</div>");
+  }
   // move to next question
-
+      questionsIndex ++;
+      console.log(questionsIndex);
   // check if we've run out of questions
+  if (questionsIndex >= questions.length) {
     // quizEnd
-  // else 
+    quizEnd();
+  }
+  else {
+      // else 
     // getQuestion
+    getQuestion ();
+  }
+
 }
 
 function quizEnd() {
   // stop timer
+  clearInterval(timerInterval);
 
   // show end screen
-
+  endScreen.removeAttribute("class");
   // show final score
+  finalScore.innerHTML = time; 
 
   // hide questions section
+  questionsEl.setAttribute("class","hide");
 }
 
-function clockTick() {
-  // update time
+// function clockTick() {
+//   // update time
 
-  // check if user ran out of time
-}
+//   // check if user ran out of time
+// }
 
 function saveHighscore() {
   // get value of input box
+  
 
   // make sure value wasn't empty
+  if (initialsEl.value === "") {
+    displayMessage("error", "Please add you initials to save score");
+
     // get saved scores from localstorage, or if not any, set to empty array
+    var lastScore = JSON.parse(localStorage.getItem("initials"));
+    finalScore.textContent = score;
 
     // format new score object for current user
+    
 
     // save to localstorage
-
+    localStorage.setItem("initials", initialsEl.value);
+    localStorage.setItem("score", finalScore);
     // redirect to next page
+    window.location.replace("highscores.html");
+
+
+  };
 }
 
 function checkForEnter(event) {
